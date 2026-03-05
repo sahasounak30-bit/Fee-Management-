aef9e257aa17e7352f34d8ff1105a8ae56616f46
xyz <xyz@gmail.com>
2026-03-05 22:04:16 +0530

    auth: 404 upgrade DONE

--- 404.php
+++ 404.php
@@ -1,12 +1,24 @@
+<?php
+include_once __DIR__ . "../server/controller/signInServer.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Error Page</title>
 </head>
+
 <body>
     <h1>Error Page</h1>
+    <?php if (!empty($_SESSION["signIn"])){?>
     <a href="/feeManager/client/views/page/feeEntry.php">Fee Entry</a>
+    <?php } else {?>
+    <a href="/feeManager/auth/signIn.php">Go to SignIn Page</a>
+    <?php } ?>
+
 </body>
+
 </html>
